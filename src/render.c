628bc41b8cc1ef622966d8d3269150914d371364
Blaemerson <blakesavage99@gmail.com>
2023-10-30 17:40:03 -0500

    Darken and lighten pixels

--- src/render.c
+++ src/render.c
@@ -15,6 +15,7 @@ void sprite_render(int x, int y, sprite *s, SDL_Texture *tex) {
                       s->width * 4);
 }
 
+// update tex with pixels at pos x, y
 void pix_buf_render(int x, int y, int w, int h, u32 *pixels, SDL_Texture *tex) {
     SDL_UpdateTexture(tex,
                       &(SDL_Rect){.x=x, .y=y, .w=w, .h=h},
@@ -42,16 +43,37 @@ void horiline(int x0, int x1, int y, u32 color, u32* pixels, int pix_buf_width) 
         pixels[(y * pix_buf_width) + x] = color;
 }
 
-u32 * clone_pixels(u32 const * src, size_t len) {
-   u32 *p = malloc(len * sizeof(u32));
-   memcpy(p, src, len * sizeof(u32));
+u32 *clone_pixels(u32 const * src, size_t len) {
+   u32 *p = malloc(len * (sizeof *p));
+   memcpy(p, src, len * (sizeof *p));
    return p;
 }
 
 inline u32 greyscale(u32 pix) {
-    u16 b = ((pix >> 16) & 0xFF);
-    u16 g = ((pix >> 8) & 0xFF);
-    u16 r = (pix & 0xFF);
-    u16 avg = (b + g + r) / 3;
-    return avg + (avg << 8) + (avg << 16);
+    u8 a = (pix >> 24) & 0xFF;
+    u8 b = (pix >> 16) & 0xFF;
+    u8 g = (pix >> 8) & 0xFF;
+    u8 r = pix & 0xFF;
+    u8 avg = (b + g + r) / 3;
+    return (a << 24) + (avg << 16) + (avg << 8) + avg;
+}
+
+inline u32 darken(u32 pix) {
+    return (pix & 0x00fefefe) >> 1;
+}
+
+inline u32 lighten(u32 pix) {
+    // Extract the alpha, blue, green, and red components
+    u8 alpha = (pix >> 24) & 0xFF;
+    u8 blue = (pix >> 16) & 0xFF;
+    u8 green = (pix >> 8) & 0xFF;
+    u8 red = pix & 0xFF;
+
+    // Lighten the color by adding a fraction of the maximum value
+    red = (u8) ((255 - red) * 0.3 + red);
+    green = (u8) ((255 - green) * 0.4 + green);
+    blue = (u8) ((255 - blue) * 0.7 + blue);
+
+    // Combine the components and return the new color
+    return ((u32)alpha << 24) | ((u32)blue << 16) | ((u32)green << 8) | (u32)red;
 }
